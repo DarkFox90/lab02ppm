5f51dbbd0e56f4265600853d503317d81070cb3e
DarkFox90 <carlospozuelos3@gmail.com>
2026-08-04 20:16:26 -0600

    implementada logica de processList

--- src/Main.kt
+++ src/Main.kt
@@ -3,7 +3,11 @@ data class ItemData(
     val originalValue: Any?,
     val type: ElementType,
     val info: String
-)
+) {
+    override fun toString(): String {
+        return "'$originalValue' estaba en la posición $originalPos, es de tipo $type e info es $info".lowercase()
+    }
+}
 
 enum class ElementType {
     CADENA,
@@ -18,6 +22,34 @@ fun main() {
 }
 
 fun processList(inputList: List<Any?>?): MutableList<ItemData>? {
-    val listaMutable = mutableListOf(1,2,3,4,5)
-    return null
+    if (inputList == null) {
+        return null
+    } else if (inputList.isEmpty()) {
+        return mutableListOf()
+    }
+
+    for (item in inputList) {
+        if (item is Int) {
+            if (item % 10 == 0 ) {
+                println("M10")
+            } else if (item % 5 == 0) {
+                println("M5")
+            } else if (item % 2 == 0) {
+                println("M2")
+            } else {
+                println("-")
+            }
+        } else if (item is String) {
+            println("L" + item.length)
+        } else if (item is Boolean) {
+            if (item == true) {
+                println("verdadero")
+            } else {
+                println("falso")
+            }
+        } else {
+            println("desconocido")
+        }
+
+    }
 }
